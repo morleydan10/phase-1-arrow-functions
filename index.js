4fdcb9222307557cd9a47078b52237a5591271c4
Daniel Morley <morleydan10@gmail.com>
2023-10-31 02:44:18 -0400

    phase-1-arrow-functions

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (number1 , number2) => number1 / number2;
+divide(2000, 100);
+
+const square = (number1) => number1 ** 2;
+square(2);
+
+const add = (num1, num2) => num1 + num2;
+add(10, 20);
